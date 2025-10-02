8986d124e337adb212750fa14cf3915374c672af
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-10-03 00:09:27 +0300

    adjust infrastructure for carrier config overrides

--- src/com/android/phone/CarrierConfigLoader.java
+++ src/com/android/phone/CarrierConfigLoader.java
@@ -836,6 +836,14 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
             loge("SubscriptionManagerService missing");
             return;
         }
+
+        int subId = SubscriptionManager.getSubscriptionId(phoneId);
+        PersistableBundle extOverrides = sm.getExtCarrierConfigOverrides(subId);
+        if (extOverrides != null) {
+            configToSend = new PersistableBundle(configToSend);
+            configToSend.putAll(extOverrides);
+        }
+
         sm.updateSubscriptionByCarrierConfig(
                 phoneId, configPackageName, configToSend,
                 () -> mHandler.obtainMessage(EVENT_SUBSCRIPTION_INFO_UPDATED, phoneId, -1)
@@ -1389,7 +1397,14 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
             if (config != null) {
                 retConfig.putAll(config);
             }
-            putAllOverrides(phoneId, retConfig);
+            putAllAospOverrides(phoneId, retConfig);
+
+            SubscriptionManagerService sm = SubscriptionManagerService.getInstance();
+            PersistableBundle extOverrides = sm.getExtCarrierConfigOverrides(subscriptionId);
+            if (extOverrides != null) {
+                retConfig.putAll(extOverrides);
+            }
+
             // Ignore the theoretical case of the default app not being present since that won't
             // work in CarrierConfigLoader today.
             final boolean allConfigsApplied =
@@ -1409,7 +1424,7 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
     /**
      * Extracted method from upstream logic.
      */
-    private void putAllOverrides(int phoneId, PersistableBundle retConfig) {
+    private void putAllAospOverrides(int phoneId, PersistableBundle retConfig) {
         PersistableBundle config = mPersistentOverrideConfigs[phoneId];
         if (config != null) {
             retConfig.putAll(config);
@@ -1470,39 +1485,10 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
         return configSubset;
     }
 
-    @Override
-    @NonNull
-    public PersistableBundle getOverrideConfigForSubIdWithFeature(int subscriptionId,
-            @NonNull String callingPackage, @Nullable String callingFeatureId) {
-        Objects.requireNonNull(callingPackage, "Calling package must be non-null");
-        enforceCallerIsSystemOrRequestingPackage(callingPackage);
-        enforceTelephonyFeatureWithException(callingPackage,
-                "getOverrideConfigForSubIdWithFeature");
-        if (!"com.android.settings".equals(callingPackage)) {
-            throw new SecurityException("not from Settings app");
-        }
-        if (!TelephonyPermissions.checkCallingOrSelfReadPhoneState(mContext, subscriptionId,
-                callingPackage, callingFeatureId, "getOverrideConfig")) {
-            return new PersistableBundle();
-        }
-
-        final int phoneId = SubscriptionManager.getPhoneId(subscriptionId);
-        if (!SubscriptionManager.isValidPhoneId(phoneId)) {
-            logd("Ignore invalid phoneId: " + phoneId + " for subId: " + subscriptionId);
-            throw new IllegalArgumentException(
-                    "Invalid phoneId " + phoneId + " for subId " + subscriptionId);
-        }
-
-        final PersistableBundle retConfig = new PersistableBundle();
-        putAllOverrides(phoneId, retConfig);
-        retConfig.remove(KEY_VERSION);
-        return retConfig;
-    }
-
     @android.annotation.EnforcePermission(android.Manifest.permission.MODIFY_PHONE_STATE)
     @Override
     public void overrideConfig(int subscriptionId, @Nullable PersistableBundle overrides,
-            boolean persistent) {
+            int type) {
         overrideConfig_enforcePermission();
         int phoneId = SubscriptionManager.getPhoneId(subscriptionId);
         if (!SubscriptionManager.isValidPhoneId(phoneId)) {
@@ -1511,34 +1497,80 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
                     "Invalid phoneId " + phoneId + " for subId " + subscriptionId);
         }
 
+        switch (type) {
+            case CarrierConfigManager.CONFIG_OVERRIDE_TYPE_AOSP_TRANSIENT:
+            case CarrierConfigManager.CONFIG_OVERRIDE_TYPE_AOSP_PERSISTENT:
+            case CarrierConfigManager.CONFIG_OVERRIDE_TYPE_GRAPHENEOS:
+                break;
+            default:
+                throw new IllegalArgumentException("invalid override type: " + type);
+        }
+
         enforceTelephonyFeatureWithException(getCurrentPackageName(), "overrideConfig");
 
         // Post to run on handler thread on which all states should be confined.
         mHandler.post(() -> {
             mNeedNotifyCallback[phoneId] = true;
-            overrideConfig(mOverrideConfigs, phoneId, overrides);
+            if (type == CarrierConfigManager.CONFIG_OVERRIDE_TYPE_AOSP_TRANSIENT ||
+                    type == CarrierConfigManager.CONFIG_OVERRIDE_TYPE_AOSP_PERSISTENT) {
+                overrideConfig(mOverrideConfigs, phoneId, overrides);
 
-            if (persistent) {
-                overrideConfig(mPersistentOverrideConfigs, phoneId, overrides);
+                if (type == CarrierConfigManager.CONFIG_OVERRIDE_TYPE_AOSP_PERSISTENT) {
+                    overrideConfig(mPersistentOverrideConfigs, phoneId, overrides);
 
-                if (overrides != null) {
-                    final CarrierIdentifier carrierId = getCarrierIdentifierForPhoneId(phoneId);
-                    saveConfigToXml(mPlatformCarrierConfigPackage, OVERRIDE_PACKAGE_ADDITION,
-                            phoneId,
-                            carrierId, mPersistentOverrideConfigs[phoneId]);
-                } else {
-                    final String iccid = getIccIdForPhoneId(phoneId);
-                    final int cid = getSpecificCarrierIdForPhoneId(phoneId);
-                    String fileName = getFilenameForConfig(mPlatformCarrierConfigPackage,
-                            OVERRIDE_PACKAGE_ADDITION, iccid, cid);
-                    File fileToDelete = new File(mContext.getFilesDir(), fileName);
-                    fileToDelete.delete();
+                    if (overrides != null) {
+                        final CarrierIdentifier carrierId = getCarrierIdentifierForPhoneId(phoneId);
+                        saveConfigToXml(mPlatformCarrierConfigPackage, OVERRIDE_PACKAGE_ADDITION,
+                                phoneId,
+                                carrierId, mPersistentOverrideConfigs[phoneId]);
+                    } else {
+                        final String iccid = getIccIdForPhoneId(phoneId);
+                        final int cid = getSpecificCarrierIdForPhoneId(phoneId);
+                        String fileName = getFilenameForConfig(mPlatformCarrierConfigPackage,
+                                OVERRIDE_PACKAGE_ADDITION, iccid, cid);
+                        File fileToDelete = new File(mContext.getFilesDir(), fileName);
+                        fileToDelete.delete();
+                    }
                 }
+            } else {
+                SubscriptionManagerService sm = SubscriptionManagerService.getInstance();
+                sm.setExtCarrierConfigOverrides(subscriptionId, overrides);
             }
-            logl("overrideConfig: subId=" + subscriptionId + ", persistent="
-                    + persistent + ", overrides=" + overrides);
+            logl("overrideConfig: subId=" + subscriptionId + ", type="
+                    + type + ", overrides=" + overrides);
             updateSubscriptionDatabase(phoneId);
         });
+    }
+
+    @Override
+    @NonNull
+    public PersistableBundle getConfigOverrides(int subscriptionId, boolean aosp) {
+        getConfigOverrides_enforcePermission();
+
+        final int phoneId = SubscriptionManager.getPhoneId(subscriptionId);
+        if (!SubscriptionManager.isValidPhoneId(phoneId)) {
+            logd("Ignore invalid phoneId: " + phoneId + " for subId: " + subscriptionId);
+            throw new IllegalArgumentException(
+                    "Invalid phoneId " + phoneId + " for subId " + subscriptionId);
+        }
+
+        PersistableBundle retConfig = new PersistableBundle();
+        if (aosp) {
+            putAllAospOverrides(phoneId, retConfig);
+            retConfig.remove(KEY_VERSION);
+        } else {
+            final long token = Binder.clearCallingIdentity();
+            try {
+                SubscriptionManagerService sm = SubscriptionManagerService.getInstance();
+                PersistableBundle configs = sm.getExtCarrierConfigOverrides(subscriptionId);
+                if (configs != null) {
+                    retConfig.putAll(configs);
+                }
+            } finally {
+                Binder.restoreCallingIdentity(token);
+            }
+        }
+        return retConfig;
     }
 
     private void overrideConfig(@NonNull PersistableBundle[] currentOverrides, int phoneId,
